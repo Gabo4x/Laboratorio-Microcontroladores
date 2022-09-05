9bf67f023b2967ffe065192a9d21b9d603450e66
Gabo4x <gabo040198@gmail.com>
2022-09-04 19:13:14 -0600

    Ultimo avance

--- Labo 1/bingo.c
+++ Labo 1/bingo.c
@@ -1,3 +1,7 @@
+//Trabajo realizado por: Gabriel Barahona Otoya B70896
+//Laboratorio de Microcontroladores IE-0624
+//Laboratorio #1
+
 #include <pic14/pic12f675.h>
  
 void delay (unsigned int tiempo);
@@ -7,8 +11,8 @@ word __at 0x2007 __CONFIG = (_WDT_OFF & _MCLRE_OFF);
 
 int n;
 int count;
-int hex1;
-int hex2;
+int num1;
+int num2;
 
 void main(void)
 {
@@ -22,328 +26,328 @@ void main(void)
 		switch (n)
 		{
 		case 0:
-			hex1 = 0x00;
-			hex2 = 0x20;
+			num1 = 0x00;
+			num2 = 0x20;
 			break;
 		case 1:
-			hex1 = 0x00;
-			hex2 = 0x21;
+			num1 = 0x00;
+			num2 = 0x21;
 			break;
 		case 2:
-			hex1 = 0x00;
-			hex2 = 0x22;
+			num1 = 0x00;
+			num2 = 0x22;
 			break;
 		case 3:
-			hex1 = 0x00;
-			hex2 = 0x23;
+			num1 = 0x00;
+			num2 = 0x23;
 			break;
 		case 4:
-			hex1 = 0x00;
-			hex2 = 0x24;
+			num1 = 0x00;
+			num2 = 0x24;
 			break;
 		case 5:
-			hex1 = 0x00;
-			hex2 = 0x25;
+			num1 = 0x00;
+			num2 = 0x25;
 			break;
 		case 6:
-			hex1 = 0x00;
-			hex2 = 0x26;
+			num1 = 0x00;
+			num2 = 0x26;
 			break;
 		case 7:
-			hex1 = 0x00;
-			hex2 = 0x27;
+			num1 = 0x00;
+			num2 = 0x27;
 			break;
 		case 8:
-			hex1 = 0x00;
-			hex2 = 0x30;
+			num1 = 0x00;
+			num2 = 0x30;
 			break;
 		case 9:
-			hex1 = 0x00;
-			hex2 = 0x31;
+			num1 = 0x00;
+			num2 = 0x31;
 			break;
 		case 10:
-			hex1 = 0x01;
-			hex2 = 0x20;
+			num1 = 0x01;
+			num2 = 0x20;
 			break;
 		case 11:
-			hex1 = 0x01;
-			hex2 = 0x21;
+			num1 = 0x01;
+			num2 = 0x21;
 			break;
 		case 12:
-			hex1 = 0x01;
-			hex2 = 0x22;
+			num1 = 0x01;
+			num2 = 0x22;
 			break;
 		case 13:
-			hex1 = 0x01;
-			hex2 = 0x23;
+			num1 = 0x01;
+			num2 = 0x23;
 			break;
 		case 14:
-			hex1 = 0x01;
-			hex2 = 0x24;
+			num1 = 0x01;
+			num2 = 0x24;
 			break;
 		case 15:
-			hex1 = 0x01;
-			hex2 = 0x25;
+			num1 = 0x01;
+			num2 = 0x25;
 			break;
 		case 16:
-			hex1 = 0x01;
-			hex2 = 0x26;
+			num1 = 0x01;
+			num2 = 0x26;
 			break;
 		case 17:
-			hex1 = 0x01;
-			hex2 = 0x27;
+			num1 = 0x01;
+			num2 = 0x27;
 			break;
 		case 18:
-			hex1 = 0x01;
-			hex2 = 0x30;
+			num1 = 0x01;
+			num2 = 0x30;
 			break;
 		case 19:
-			hex1 = 0x01;
-			hex2 = 0x31;
+			num1 = 0x01;
+			num2 = 0x31;
 			break;
 		case 20:
-			hex1 = 0x02;
-			hex2 = 0x20;
+			num1 = 0x02;
+			num2 = 0x20;
 			break;
 		case 21:
-			hex1 = 0x02;
-			hex2 = 0x21;
+			num1 = 0x02;
+			num2 = 0x21;
 			break;
 		case 22:
-			hex1 = 0x02;
-			hex2 = 0x22;
+			num1 = 0x02;
+			num2 = 0x22;
 			break;
 		case 23:
-			hex1 = 0x02;
-			hex2 = 0x23;
+			num1 = 0x02;
+			num2 = 0x23;
 			break;
 		case 24:
-			hex1 = 0x02;
-			hex2 = 0x24;
+			num1 = 0x02;
+			num2 = 0x24;
 			break;
 		case 25:
-			hex1 = 0x02;
-			hex2 = 0x25;
+			num1 = 0x02;
+			num2 = 0x25;
 			break;
 		case 26:
-			hex1 = 0x02;
-			hex2 = 0x26;
+			num1 = 0x02;
+			num2 = 0x26;
 			break;
 		case 27:
-			hex1 = 0x02;
-			hex2 = 0x27;
+			num1 = 0x02;
+			num2 = 0x27;
 			break;
 		case 28:
-			hex1 = 0x02;
-			hex2 = 0x30;
+			num1 = 0x02;
+			num2 = 0x30;
 			break;
 		case 29:
-			hex1 = 0x02;
-			hex2 = 0x31;
+			num1 = 0x02;
+			num2 = 0x31;
 			break;
 		case 30:
-			hex1 = 0x03;
-			hex2 = 0x20;
+			num1 = 0x03;
+			num2 = 0x20;
 			break;
 		case 31:
-			hex1 = 0x03;
-			hex2 = 0x21;
+			num1 = 0x03;
+			num2 = 0x21;
 			break;
 		case 32:
-			hex1 = 0x03;
-			hex2 = 0x22;
+			num1 = 0x03;
+			num2 = 0x22;
 			break;
 		case 33:
-			hex1 = 0x03;
-			hex2 = 0x23;
+			num1 = 0x03;
+			num2 = 0x23;
 			break;
 		case 34:
-			hex1 = 0x03;
-			hex2 = 0x24;
+			num1 = 0x03;
+			num2 = 0x24;
 			break;
 		case 35:
-			hex1 = 0x03;
-			hex2 = 0x25;
+			num1 = 0x03;
+			num2 = 0x25;
 			break;
 		case 36:
-			hex1 = 0x03;
-			hex2 = 0x26;
+			num1 = 0x03;
+			num2 = 0x26;
 			break;
 		case 37:
-			hex1 = 0x03;
-			hex2 = 0x27;
+			num1 = 0x03;
+			num2 = 0x27;
 			break;
 		case 38:
-			hex1 = 0x03;
-			hex2 = 0x30;
+			num1 = 0x03;
+			num2 = 0x30;
 			break;
 		case 39:
-			hex1 = 0x03;
-			hex2 = 0x31;
+			num1 = 0x03;
+			num2 = 0x31;
 			break;
 		case 40:
-			hex1 = 0x04;
-			hex2 = 0x20;
+			num1 = 0x04;
+			num2 = 0x20;
 			break;
 		case 41:
-			hex1 = 0x04;
-			hex2 = 0x21;
+			num1 = 0x04;
+			num2 = 0x21;
 			break;
 		case 42:
-			hex1 = 0x04;
-			hex2 = 0x22;
+			num1 = 0x04;
+			num2 = 0x22;
 			break;
 		case 43:
-			hex1 = 0x04;
-			hex2 = 0x23;
+			num1 = 0x04;
+			num2 = 0x23;
 			break;
 		case 44:
-			hex1 = 0x04;
-			hex2 = 0x24;
+			num1 = 0x04;
+			num2 = 0x24;
 			break;
 		case 45:
-			hex1 = 0x04;
-			hex2 = 0x25;
+			num1 = 0x04;
+			num2 = 0x25;
 			break;
 		case 46:
-			hex1 = 0x04;
-			hex2 = 0x26;
+			num1 = 0x04;
+			num2 = 0x26;
 			break;
 		case 47:
-			hex1 = 0x04;
-			hex2 = 0x27;
+			num1 = 0x04;
+			num2 = 0x27;
 			break;
 		case 48:
-			hex1 = 0x04;
-			hex2 = 0x30;
+			num1 = 0x04;
+			num2 = 0x30;
 			break;
 		case 49:
-			hex1 = 0x04;
-			hex2 = 0x31;
+			num1 = 0x04;
+			num2 = 0x31;
 			break;
 		case 50:
-			hex1 = 0x05;
-			hex2 = 0x20;
+			num1 = 0x05;
+			num2 = 0x20;
 			break;
 		case 51:
-			hex1 = 0x05;
-			hex2 = 0x21;
+			num1 = 0x05;
+			num2 = 0x21;
 			break;
 		case 52:
-			hex1 = 0x05;
-			hex2 = 0x22;
+			num1 = 0x05;
+			num2 = 0x22;
 			break;
 		case 53:
-			hex1 = 0x05;
-			hex2 = 0x23;
+			num1 = 0x05;
+			num2 = 0x23;
 			break;
 		case 54:
-			hex1 = 0x05;
-			hex2 = 0x24;
+			num1 = 0x05;
+			num2 = 0x24;
 			break;
 		case 55:
-			hex1 = 0x05;
-			hex2 = 0x25;
+			num1 = 0x05;
+			num2 = 0x25;
 			break;
 		case 56:
-			hex1 = 0x05;
-			hex2 = 0x26;
+			num1 = 0x05;
+			num2 = 0x26;
 			break;
 		case 57:
-			hex1 = 0x05;
-			hex2 = 0x27;
+			num1 = 0x05;
+			num2 = 0x27;
 			break;
 		case 58:
-			hex1 = 0x05;
-			hex2 = 0x30;
+			num1 = 0x05;
+			num2 = 0x30;
 			break;
 		case 59:
-			hex1 = 0x05;
-			hex2 = 0x31;
+			num1 = 0x05;
+			num2 = 0x31;
 			break;
 		case 60:
-			hex1 = 0x06;
-			hex2 = 0x20;
+			num1 = 0x06;
+			num2 = 0x20;
 			break;
 		case 61:
-			hex1 = 0x06;
-			hex2 = 0x21;
+			num1 = 0x06;
+			num2 = 0x21;
 			break;
 		case 62:
-			hex1 = 0x06;
-			hex2 = 0x22;
+			num1 = 0x06;
+			num2 = 0x22;
 			break;
 		case 63:
-			hex1 = 0x06;
-			hex2 = 0x23;
+			num1 = 0x06;
+			num2 = 0x23;
 			break;
 		case 64:
-			hex1 = 0x06;
-			hex2 = 0x24;
+			num1 = 0x06;
+			num2 = 0x24;
 			break;
 		case 65:
-			hex1 = 0x06;
-			hex2 = 0x25;
+			num1 = 0x06;
+			num2 = 0x25;
 			break;
 		case 66:
-			hex1 = 0x06;
-			hex2 = 0x26;
+			num1 = 0x06;
+			num2 = 0x26;
 			break;
 		case 67:
-			hex1 = 0x06;
-			hex2 = 0x27;
+			num1 = 0x06;
+			num2 = 0x27;
 			break;
 		case 68:
-			hex1 = 0x06;
-			hex2 = 0x30;
+			num1 = 0x06;
+			num2 = 0x30;
 			break;
 		case 69:
-			hex1 = 0x06;
-			hex2 = 0x31;
+			num1 = 0x06;
+			num2 = 0x31;
 			break;
 		case 70:
-			hex1 = 0x07;
-			hex2 = 0x20;
+			num1 = 0x07;
+			num2 = 0x20;
 			break;
 		case 71:
-			hex1 = 0x07;
-			hex2 = 0x21;
+			num1 = 0x07;
+			num2 = 0x21;
 			break;
 		case 72:
-			hex1 = 0x07;
-			hex2 = 0x22;
+			num1 = 0x07;
+			num2 = 0x22;
 			break;
 		case 73:
-			hex1 = 0x07;
-			hex2 = 0x23;
+			num1 = 0x07;
+			num2 = 0x23;
 			break;
 		case 74:
-			hex1 = 0x07;
-			hex2 = 0x24;
+			num1 = 0x07;
+			num2 = 0x24;
 			break;
 		case 75:
-			hex1 = 0x07;
-			hex2 = 0x25;
+			num1 = 0x07;
+			num2 = 0x25;
 			break;
 		case 76:
-			hex1 = 0x07;
-			hex2 = 0x26;
+			num1 = 0x07;
+			num2 = 0x26;
 			break;
 		case 77:
-			hex1 = 0x07;
-			hex2 = 0x27;
+			num1 = 0x07;
+			num2 = 0x27;
 			break;
 		case 78:
-			hex1 = 0x07;
-			hex2 = 0x30;
+			num1 = 0x07;
+			num2 = 0x30;
 			break;
 		case 79:
-			hex1 = 0x07;
-			hex2 = 0x31;
+			num1 = 0x07;
+			num2 = 0x31;
 			break;
 		case 80:
-			hex1 = 0x10;
-			hex2 = 0x20;
+			num1 = 0x10;
+			num2 = 0x20;
 			break;
 		default:
 			n = 0;
@@ -362,9 +366,9 @@ void main(void)
 	{
 		if(count<=16)
 		{
-			GPIO = hex1;
+			GPIO = num1;
 			delay(time);
-			GPIO = hex2;
+			GPIO = num2;
 			delay(time);
 		}
 		if(count>16)
