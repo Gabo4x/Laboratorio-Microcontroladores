dc5d2f7eb3d6eb5202ad523c768d11af7a21320a
Gabo4x <gabo040198@gmail.com>
2022-09-20 21:19:44 -0600

    Avance final Laboratorio 2

--- L2_B70896/src/semaforo.c
+++ L2_B70896/src/semaforo.c
@@ -1,22 +1,31 @@
+//Trabajo realizado por: Gabriel Barahona Otoya B70896
+//Laboratorio de Microcontroladores IE-0624
+//Laboratorio #2
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
 
 ISR (INT0_vect)
 {
-  PORTB ^= (1<<PB1);
+    PORTB ^= ~(1<<PB0) | ~(1<<PB3) | ~(1<<PB5);
 }
 ISR (INT1_vect)
 {
-  PORTB ^= (1<<PB0);
+    PORTB &= ~(1<<PB1) & ~(1<<PB2) & ~(1<<PB4);
+}
+ISR (PCINT0_vect)
+{
+    PORTB ^= ~(1<<PB6);
 }
 
-void external_interrupt()
+void config()
 {
   DDRB |= (1<<PB0) | (1<<PB1) | (1<<PB2) | (1<<PB3) | (1<<PB4)| (1<<PB5);
   sei();
   GIMSK |= (1<<INT0);
   MCUCR |= (1<<ISC01);
+  GIMSK |= (1<<INT1);
+  MCUCR |= (1<<ISC11);
 }
 
 void pin_change_interrupt()
@@ -28,7 +37,7 @@ void pin_change_interrupt()
 
 int main()
 {
-  external_interrupt();
+  config();
   pin_change_interrupt();
   while(1)
   {
